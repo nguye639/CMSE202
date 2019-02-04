b7a2a13d3b80d5bf88552fa05a814f809790b7ef
Devin Silvia <devin.silvia@gmail.com>
2019-02-04 15:00:35 -0500

    Adding the pendulum_functions script to HW-02 directory

---
+++ Homework/HW-02/pendulum_functions.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# These functions are guaranteed to be bug free!
+# You don't need to look for bugs here, but you are free to modify these functions
+# If you mess these functions up, don't forget about "git checkout" for individual files
+
+# Pendulum rod lengths (m), bob masses (kg).
+L1, L2 = 1, 1
+m1, m2 = 1, 1
+# The gravitational acceleration (m.s-2).
+g = 9.81
+
+def deriv(y, t):
+    #"""Return the first derivatives of y = theta1, z1, theta2, z2."""
+    theta1, z1, theta2, z2 = y
+
+    c, s = np.cos(theta1-theta2), np.sin(theta1-theta2)
+
+    theta1dot = z1
+    z1dot = (m2*g*np.sin(theta2)*c - m2*s*(L1*z1**2*c + L2*z2**2) -
+             (m1+m2)*g*np.sin(theta1)) / L1 / (m1 + m2*s**2)
+    theta2dot = z2
+    z2dot = ((m1+m2)*(L1*z1**2*s - g*np.sin(theta2) + g*np.sin(theta1)*c) + 
+             m2*L2*z2**2*s*c) / L2 / (m1 + m2*s**2)
+    return theta1dot, z1dot, theta2dot, z2dot
+
+def calc_E(y):
+    #"""Return the total energy of the system."""
+
+    th1, th1d, th2, th2d = y.T
+    V = -(m1+m2)*L1*g*np.cos(th1) - m2*L2*g*np.cos(th2)
+    T = 0.5*m1*(L1*th1d)**2 + 0.5*m2*((L1*th1d)**2 + (L2*th2d)**2 +
+            2*L1*L2*th1d*th2d*np.cos(th1-th2))
+    return T + V
